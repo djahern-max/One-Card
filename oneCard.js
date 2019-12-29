1f7d9cd42cafbde0dd971b26edcbecf4a538584d
Dano <danielaherniv@gmail.com>
2019-12-29 11:24:34 -0500

    working on joining tables

--- oneCard.js
+++ oneCard.js
@@ -23,16 +23,24 @@ let runSearch = function() {
       message: 'What would you like to do?',
       choices: [
         'Find transaction by name',
-        'Find transactions within a specific range'
+        'Find vendor by merchant name',
+        'Find transactions by merchant name',
+        'Find transaction and department of employee'
       ]
     })
     .then(function(answer) {
-      switch (answer) {
+      switch (answer.action) {
         case 'Find transaction by name':
           transactionSearch();
           break;
-        case 'Find transactions within a specific range':
-          rangeSearch();
+        case 'Find vendor by merchant name':
+          merchantSearch();
+          break;
+        case 'Find transactions by merchant name':
+          merchantTransSearch();
+          break;
+        case 'Find transaction and department of employee':
+          transAndDepartmentSearch();
           break;
       }
     });
@@ -41,27 +49,101 @@ let runSearch = function() {
 let transactionSearch = function() {
   inquirer
     .prompt({
-      name: 'name',
+      name: 'Name',
       type: 'input',
-      message: 'Whose transactions would you like to see?'
+      message: 'What cardholder transactions would you like to see?'
     })
     .then(function(answer) {
       let query =
-        'SELECT Name, Posting_Date, Source_Currency_Amount FROM one_card WHERE ?';
-      connection.query(query, { oneCard: answer.oneCard }, function(err, res) {
+        'SELECT Name, Source_Currency_Amount, Merchant_Name FROM one_card WHERE ?';
+      connection.query(query, { Name: answer.Name }, function(err, res) {
         for (let i = 0; i < res.length; i++) {
           console.log(
             'Name: ' +
               res[i].Name +
-              '\nPosting Date: ' +
-              res[i].Posting_Date +
               '\nAmount: ' +
               res[i].Source_Currency_Amount +
+              '\nMerchant Name: ' +
+              res[i].Merchant_Name +
               '\n--------\n'
           );
         }
 
         runSearch();
       });
+    });
+};
+
+function merchantSearch() {
+  inquirer
+    .prompt({
+      name: 'Merchant_Name',
+      type: 'input',
+      message: 'What Merchant would you like to look for?'
+    })
+    .then(function(answer) {
+      console.log(answer.Merchant_Name);
+      connection.query(
+        'SELECT * FROM one_card WHERE ?',
+        { Merchant_Name: answer.Merchant_Name },
+        function(err, res) {
+          console.log(
+            'Merchant_Name: ' +
+              res[0].Merchant_Name +
+              ' || Merchant_City: ' +
+              res[0].Merchant_City +
+              ' || Merchant_State: ' +
+              res[0].Merchant_State
+          );
+          runSearch();
+        }
+      );
+    });
+}
+
+let merchantTransSearch = function() {
+  inquirer
+    .prompt({
+      name: 'Merchant_Name',
+      type: 'input',
+      message: 'Which Merchant transactions would you like to see?'
+    })
+    .then(function(answer) {
+      let query =
+        'SELECT Merchant_Name, Source_Currency_Amount, Posting_Date, Name FROM one_card WHERE ?';
+      connection.query(query, { Merchant_Name: answer.Merchant_Name }, function(
+        err,
+        res
+      ) {
+        for (let i = 0; i < res.length; i++) {
+          console.log(
+            'Merchant_Name: ' +
+              res[i].Merchant_Name +
+              '\nAmount: ' +
+              res[i].Source_Currency_Amount +
+              '\nPosting_Date: ' +
+              res[i].Posting_Date +
+              '\nName: ' +
+              res[i].Name +
+              '\n--------\n'
+          );
+        }
+        runSearch();
+      });
+    });
+};
+
+let transAndDepartmentSearch = function() {
+  inquirer
+    .prompt({
+      name: 'Name',
+      type: 'input',
+      message:
+        'What cardholder transactions and department would you like to see?'
+    })
+    .then(function(answer) {
+      let query =
+        'SELECT employee_data.Name,employee_data.Department FROM employee_data';
+      query += 'INNER JOIN ';
     });
 };
